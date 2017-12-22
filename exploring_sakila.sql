1e27389616b4a7c221f7d2c569445ee57048c67b
Mark Groner <markgroner@utexas.edu>
2017-12-22 17:33:58 -0600

    added answers to 1 and 2

--- exploring_sakila.sql
+++ exploring_sakila.sql
@@ -1,17 +1,28 @@
 ## 1a. Display the first and last names of all actors from the table `actor`
-
+select first_name, last_name from actor;
 
 ## 1b. Display the first and last name of each actor in a single column in upper case letters. Name the column `Actor Name`.
+select upper(concat(first_name, ' ', last_name)) as `Actor Name` from actor;
 
 ## 2a. You need to find the ID number, first name, and last name of an actor, of whom you know only the first name, "Joe." What is one query would you use to obtain this information?
+select actor_id, first_name, last_name from actor where first_name = 'Joe';
 
 ## 2b. Find all actors whose last name contain the letters `GEN`:
+## Answer assumes `GEN` must be in order, but that it is not case sensitive
+select * from actor where upper(last_name) like '%gen%';
 
 ## 2c. Find all actors whose last names contain the letters `LI`. This time, order the rows by last name and first name, in that order:
+## Answer assumes `LI` must be in order, but that it is not case sensitive
+select * from actor where UPPER(last_name) like '%LI%'
+order by last_name, first_name;
 
 ## 2d. Using `IN`, display the `country_id` and `country` columns of the following countries: Afghanistan, Bangladesh, and China:
+select country_id, country from country
+where country in ('Afghanistan', 'Bangladesh', 'China');
 
 ## 3a. Add a `middle_name` column to the table `actor`. Position it between `first_name` and `last_name`. Hint: you will need to specify the data type.
+
+
 
 ## 3b. You realize that some of these actors have tremendously long last names. Change the data type of the `middle_name` column to `blobs`.
 
